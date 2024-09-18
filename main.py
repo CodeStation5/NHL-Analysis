aa21d0e08c4949297a673067686ec308a926df22
Jerry Sandhu <jsa2282@wgu.edu>
2024-09-18 02:35:59 -0700

    Add standings and player stats

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from requests import get
 from pprint import PrettyPrinter
 
 BASE_URL = "https://api-web.nhle.com"
-Today_JSON = "/v1/schedule/now"
+TODAY_SCHEDULE_JSON = "/v1/schedule/now"
+STANDINGS_NOW_JSON = "/v1/standings/now"
+PLAYER_STATS_JSON = "https://api-web.nhle.com/v1/player/" #/PlayerID/landing
 
 printer = PrettyPrinter()
 
-data = get(BASE_URL + Today_JSON).json()
+data = get(BASE_URL + TODAY_SCHEDULE_JSON).json()
 printer.pprint(data)
